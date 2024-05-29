1efae6cc50b078d14002c00f8c2ae715aea3a3e2
shashanksola <solashashank1010@gmail.com>
2024-05-29 22:43:14 +0530

    initial commit

---
+++ build-server/script.js
@@ -0,0 +1,67 @@
+const { exec } = require('child_process');
+const { readdirSync, lstatSync, createReadStream } = require('fs');
+const path = require('path');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const mime = require('mime-types');
+const { error } = require('console');
+
+const s3Client = new S3Client({
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY
+    }
+})
+
+const PROJECT_ID = process.env.PROJECT_ID;
+
+async function init() {
+    console.log(`Reading Repository...`);
+
+    const outDirPath = path.join(__dirname, 'output');
+
+    const p = exec(`cd ${outDirPath} && npm install && npm run build`);
+
+    p.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    p.stderr.on('error', (error) => {
+        console.log(`Error : ${error}`);
+    })
+
+    p.on('close', async () => {
+        console.log("Build Successful");
+
+        let distFolderPath;
+        let distFolderContents;
+
+        try {
+            distFolderPath = path.join(__dirname, 'output', 'dist');
+            distFolderContents = readdirSync(distFolderPath, { recursive: true });
+        } catch (e) {
+            console.error(`Error: Cannot find dist folder, exiting...`)
+            process.exit(1);
+        }
+
+        console.log('Starting to Upload')
+        for (const file of distFolderContents) {
+            const filePath = path.join(distFolderPath, file);
+            if (lstatSync(filePath).isDirectory()) continue;
+            console.log('Uploading ', filePath);
+
+            const command = new PutObjectCommand({
+                Bucket: process.env.BUCKET_NAME,
+                Key: `__outputs/${PROJECT_ID}/${file}`,
+                Body: createReadStream(filePath),
+                ContentType: mime.lookup(filePath)
+            });
+
+            await s3Client.send(command);
+        }
+
+        console.log(`S3 Upload Successfull`);
+    })
+}
+
+init();
